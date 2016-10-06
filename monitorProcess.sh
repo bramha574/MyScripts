7da1cfe70f548deb4cdcdde295b79d62017e0366
bramha574 <nvbramhavardhan@gmail.com>
2016-10-06 17:29:19 -0500

    Update monitorProcess.sh

--- monitorProcess.sh
+++ monitorProcess.sh
@@ -3,11 +3,17 @@
 
 #This script monitors the process in which we need to pass the process name as a parameter eg:    sudo sh monitorProcess.sh apache2
 
-SERVICE="$1"
-RESULT=`ps -a | sed -n /${SERVICE}/p`
+#!/bin/sh
 
-if [ "${RESULT:-null}" = null ]; then
-    echo "not running"
-else
-    echo "running"
-fi
+PROCESS="$1"
+PROCANDARGS=$*
+
+    RESULT=`pgrep ${PROCESS}`
+
+    if [ "${RESULT:-null}" = null ]; then
+            echo "${PROCESS} not running, starting "$PROCANDARGS
+            $PROCANDARGS &
+    else
+            echo "running"
+    fi
+
