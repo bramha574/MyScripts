1ae8a43a2e4bec0231b42131ae97c267c6900175
bramha574 <nvbramhavardhan@gmail.com>
2016-10-24 16:10:34 -0500

    Create deployWar.sh

---
+++ deployWar.sh
@@ -0,0 +1,24 @@
+TOMCATLOC="apache-tomcat-6"    # path of tomcat directory
+WAR_FILE=$1                     # argument that has to contain war file.
+
+#validating parameters
+if [ $# -ne 1 ] ; then
+        echo "Illegal Number of parameters.  You should run script as <scriptname> <path/warfile.war>"
+        exit 1
+fi
+
+#checking whether user has write permissions
+if [ ! -w $TOMCATLOC -o ! -w $TOMCATLOC/webapps ]; then
+    echo "$TOMCATLOC and $TOMCATLOC/webapps should have write permissions" 1>&2
+    exit 1
+fi
+
+# installing war into tomcat webapps
+if [ ! -r $WAR_FILE ]; then
+    echo "$WAR_FILE could not be found. Check the war file location and try again." 1>&2
+else
+    cp $WAR_FILE $TOMCAT_WEBAPPS
+fi
+
+#Starting tomcat after deploying the war
+$TOMCATLOC/bin/startup.sh
